bebb328d503442598568561a08217d75df1427d4
veloce-dev <288309926+trytheveloce@users.noreply.github.com>
2026-05-20 10:33:13 +0000

    feat: wire up commander entrypoint

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { registerStatus } from './commands/status';
+import { registerNodes } from './commands/nodes';
+import { registerYield } from './commands/yield';
+import { registerBalance } from './commands/balance';
+import { registerClaim } from './commands/claim';
+import { registerConnect } from './commands/connect';
+
+const program = new Command();
+
+program
+  .name('velocy')
+  .description('Command-line interface for the Veloce compute marketplace')
+  .version('0.1.0');
+
+registerStatus(program);
+registerNodes(program);
+registerYield(program);
+registerBalance(program);
+registerClaim(program);
+registerConnect(program);
+
+program.parseAsync(process.argv).catch((err) => {
+  console.error(err.message || err);
+  process.exit(1);
+});
